fbdae51dbfdb0730e924689d313434bb7909ef09
son <thsgur1212@naver.com>
2023-01-11 22:48:38 +0900

    2023/01/11 commit

--- src/main/java/springSTD/study/controller/HelloController.java
+++ src/main/java/springSTD/study/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("hello/") // get/post 방식에서 get을 의미함 8080/뒤에 오는 url을 구분
     public String hello(Model model){
-        model.addAttribute("data","spring!!");
+        model.addAttribute("data","spring!!"); // spring!! 이 값이 hello.html에서 ${data}부분에 들어감.
         return "hello"; // templates 디렉토리 내부에 hello.html 찾아간 것
         // return시에는 항상 viewResolver가 templates 내부에서 html파일을 찾음(spring 구조가 그렇게 짜임)
     }
