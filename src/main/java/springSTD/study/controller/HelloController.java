2dea620be32167e9accc6723a1c742ee4f96a5c3
son <thsgur1212@naver.com>
2023-01-13 19:02:40 +0900

    2023/01/13 commit

--- src/main/java/springSTD/study/controller/HelloController.java
+++ src/main/java/springSTD/study/controller/HelloController.java
@@ -4,6 +4,8 @@ package springSTD.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +21,79 @@ public class HelloController {
     public String sonny(Model model) {
         model.addAttribute("data", "sonny! welcome!!");
         return "sonny";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        //@RequestParam = 웹에서 파라미터 받는 어노테이션
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-mvc-t")
+    public String helloMvct(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 head와 body 중에서 body에 직접 데이터를 넣겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello! " + name;
+    }
+
+    @GetMapping("hello-string-t")
+    @ResponseBody
+    public String helloStringt(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //Hello class 객체 생성 (스프링은 객체를 생성하면 객체에서 생성되는 데이터는 json형태로 받음)
+        hello.setName(name); // 파라미터로 받은 데이터를 이름으로 설정
+        return hello; // 설정된 이름 반환
+    }
+
+    static class Hello { // Hello 클래스
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GetMapping("hello-api-t")
+    public String helloTestApi(@RequestParam("name") String name, Model model){ // 받은 파라미터를 모델에 저장하여 getter setter 사용해서
+        Hellotest hellot = new Hellotest();                                     // name 설정과 모델에 추가, 그 후 페이지에 띄워줌
+        hellot.setNames(name);
+        model.addAttribute("name",hellot.getNames());
+        return "hello-template";
+    }
+
+        static class Hellotest{ // getter setter 사용할 클래스 생성
+            private String names;
+
+            public String getNames() {
+                return names;
+            }
+
+            public void setNames(String names) {
+                this.names = names;
+            }
+        }
+
+
+    @GetMapping("ht")
+    @ResponseBody
+    public Hellotest ht(@RequestParam("name") String name){
+        Hellotest hts = new Hellotest();
+        hts.setNames(name);
+        return hts;
     }
 }
